1b473df6b881986e81f7afd32a37a3e88871cee7
MeuReek <94654740+MeuReek@users.noreply.github.com>
2022-05-01 10:46:41 -0300

    Desafío 3 - Ardiles

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const Contenedor = require('./main')
 const PORT = 8080
 
 const app = express()
+const contenedor = new Contenedor('/productos.txt')
 
 const server = app.listen(PORT, () => {
     console.log('Servidor HTTP escuchando en el puerto ' + PORT)
@@ -14,14 +15,14 @@ app.get('/', (req, res) => {
 })
 
 app.get('/productos', (req, res) => {
-    const productos = Contenedor.getAll()
+    const productos = contenedor.getAll()
     res.send(`<h2>Lista de productos obtenidos: </h2>
             <p>${productos}</p>`)
 })
 
 app.get('/productoRandom', (req, res) => {
-    const producto = Contenedor.getRandom()
+    const productoRandom = contenedor.getRandom()
     res.send(`<h2>Producto obtenido de forma aleatoria: </h2>
-            <p>${producto}</p>`)
+            <p>${productoRandom}</p>`)
 })
 
