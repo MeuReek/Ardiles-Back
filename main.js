2e679605cd9359b6137f42e5078889dc4c785516
David Alvarez <aalvag@live.com>
2022-05-04 20:49:07 -0300

    correciones

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 const fs = require("fs");
 
-
 class Contenedor {
   constructor(ruta) {
     this.ruta = ruta;
@@ -9,37 +8,32 @@ class Contenedor {
   async getAll() {
     try {
       const contenido = await fs.promises.readFile("./productos.txt", "utf8");
-      const infoText = JSON.stringify(contenido, null, 2)
-      const info = JSON.parse(infoText)
-      return info
+      const infoText = JSON.stringify(contenido, null, 2);
+      const info = JSON.parse(infoText);
+      return info;
     } catch (error) {
-      return error
+      return error;
     }
   }
-
 
   async getRandom() {
     try {
       const contenido = await fs.promises.readFile("./productos.txt", "utf8");
-      const productos = JSON.parse(contenido)
-      const indice = Math.floor(Math.random() * productos.length());
+      const productos = JSON.parse(contenido);
+      const indice = Math.floor(Math.random() * productos.length);
       const productoRandom = productos[indice];
-      return productoRandom
+      return productoRandom;
     } catch (error) {
-        return error
+      return error;
     }
   }
-
- 
-
 }
 
-
- async function main() {
-   const producto = new Contenedor("./productos.txt");
-   await producto.getAll();
-   await producto.getRandom();
+async function main() {
+  const producto = new Contenedor("./productos.txt");
+  await producto.getAll();
+  await producto.getRandom();
 }
-main()  
+main();
 
 module.exports = Contenedor;
--- server.js
+++ server.js
@@ -1,27 +1,26 @@
-const express = require('express')
-const Contenedor = require('./main')
+const express = require("express");
+const Contenedor = require("./main");
 
-const PORT = 8080
+const PORT = 8080;
 
-const app = express()
-const contenedor = new Contenedor('/productos.txt')
+const app = express();
+const contenedor = new Contenedor("/productos.txt");
 
 const server = app.listen(PORT, () => {
-    console.log('Servidor HTTP escuchando en el puerto ' + PORT)
-})
+  console.log("Servidor HTTP escuchando en el puerto " + PORT);
+});
 
-app.get('/', (req, res) => {
-    res.send('<h1>Servidor de Productos</h1>')
-})
+app.get("/", (req, res) => {
+  res.send("<h1>Servidor de Productos</h1>");
+});
 
-app.get('/productos', async (req, res) => {
-    const productos = await contenedor.getAll()
-    res.send(`<h2>Lista de productos obtenidos: </h2>
-            <p>${productos}</p>`)
-})
+app.get("/productos", async (req, res) => {
+  const productos = await contenedor.getAll();
+  res.send(`<h2>Lista de productos obtenidos: </h2>
+            <p>${productos}</p>`);
+});
 
-app.get('/productoRandom', async (req, res) => {
-    const productoRandom = await contenedor.getRandom()
-    res.send('<h2>Producto obtenido de forma aleatoria: </h2>'+productoRandom)
-})
-
+app.get("/productoRandom", async (req, res) => {
+  const productoRandom = await contenedor.getRandom();
+  res.send(productoRandom);
+});
