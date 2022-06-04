9dbf4839f405135564c45329102322e8dd02c60f
MeuReek <94654740+MeuReek@users.noreply.github.com>
2022-06-04 01:45:02 -0300

    Pug

--- Desafio5-Pug/servidor.js
+++ Desafio5-Pug/servidor.js
@@ -1,20 +1,23 @@
 const express = require('express')
 
 const app = express()
+const PORT = 8080;
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 
 app.set('views', './views')
 
 app.set('view engine', 'pug')
 
-app.get('/', (req, res) => {
-    res.render('inicio', { productos })
-})
+const productos = [];
+
+app.get('/', (re, res) => {
+    res.render('inicio', {productos})
+});
 
 app.post('/productos', (req, res) => {
     productos.push(req.body)
     res.redirect('/')
 })
 
-const server = app.listen(8080, () => {
-    console.log('Servidor esta corriendo satisfactoriamente')
-})
+app.listen(PORT, () => { console.log('Servidor esta corriendo satisfactoriamente en puerto' + PORT)});
